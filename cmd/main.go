7ad7135901daab1acd918127a66578148eafcc7b
Elijah Zobenko <i.zobenko@corp.mail.ru>
2021-09-16 22:23:06 +0300

    Initialize GQLGen

---
+++ cmd/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	graph2 "He110/PersonalWebSite/internal/graph"
+	generated2 "He110/PersonalWebSite/internal/graph/generated"
+
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(generated2.NewExecutableSchema(generated2.Config{Resolvers: &graph2.Resolver{}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
--- go.mod
+++ go.mod
@@ -3,11 +3,9 @@ module He110/PersonalWebSite
 go 1.16
 
 require (
-	github.com/99designs/gqlgen v0.14.0 // indirect
-	github.com/caarlos0/env v3.5.0+incompatible // indirect
-	github.com/go-chi/chi v3.3.2+incompatible // indirect
-	github.com/urfave/cli v1.20.0 // indirect
-	github.com/vektah/gqlparser v1.2.0 // indirect
-	go.uber.org/dig v1.12.0 // indirect
-	go.uber.org/zap v1.19.1 // indirect
+	github.com/99designs/gqlgen v0.14.0
+	github.com/stretchr/testify v1.7.0 // indirect
+	github.com/vektah/gqlparser/v2 v2.2.0
+	gopkg.in/yaml.v2 v2.2.8 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
